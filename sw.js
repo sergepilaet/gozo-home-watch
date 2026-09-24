719819efa2d4783f8029721af6e74a91d2d3ab60
sergepilaet <sergepilaet@gmail.com>
2026-09-24 14:47:28 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+  if(e.request.mode==="navigate"){
+    e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match(e.request)));
+  }
+});
